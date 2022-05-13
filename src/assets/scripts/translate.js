4a032b123d3ded1c4d74bd7943aba242501b08e6
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-05-13 10:45:29 +0300

    feat: add translate.js

---
+++ src/assets/scripts/translate.js
@@ -0,0 +1,10 @@
+const i18Obj = {
+  'en': {
+    'about': 'I have been interested in IT for a long time. I recently began to be fascinated by building websites. I am keen to gain more experience in the field. For this reason, I am looking for a company willing to offer me a placement among their developers. In return, I would offer my full commitment, and be a pleasant and friendly addition to your team.',
+  },
+  'ru': {
+    'about': 'Я давно интересуюсь ИТ. Недавно я начала увлекаться созданием веб-сайтов. Я стремлюсь получить больше опыта в этой области. По этой причине я ищу компанию, готовую предложить мне место среди своих разработчиков. Взамен я бы предложила свою полную приверженность и была бы приятным и дружелюбным дополнением к вашей команде.',
+  }
+}
+
+export default i18Obj;
