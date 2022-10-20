a92b550395017678acee0c2bb62573d6d183ffe2
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-10-20 10:59:43 +0300

    fix: update info

--- src/assets/scripts/translate.js
+++ src/assets/scripts/translate.js
@@ -1,9 +1,9 @@
 const i18Obj = {
   'en': {
-    'about': 'I have been interested in IT for a long time. I recently began to be fascinated by building websites. I am keen to gain more experience in the field. For this reason, I am looking for a company willing to offer me a placement among their developers. In return, I would offer my full commitment, and be a pleasant and friendly addition to your team.',
+    'about': 'I am looking for a job as a html coder, in this profession I have found application of my skills: assiduity, attention to details. Besides I like coding, I can work for hours, forgetting to take a break for tea. In a few years I see myself as a front-end developer. For that, I`m improving my English and learning JavaScript. Here`s a little memory <a href="https://kseniyayaskevich.github.io/kseniyayaskevich-JSFEPRESCHOOL/memory-game/" target="_blank">GAME</a>. I am keen to gain more experience in the field. For this reason, I am looking for a company willing to offer me a placement among their developers. In return, I would offer my full commitment and be a pleasant and friendly addition to your team.',
   },
   'ru': {
-    'about': 'Я давно интересуюсь ИТ. Недавно я начала увлекаться созданием веб-сайтов. Я стремлюсь получить больше опыта в этой области. По этой причине я ищу компанию, готовую предложить мне место среди своих разработчиков. Взамен я бы предложила свою полную приверженность и была бы приятным и дружелюбным дополнением к вашей команде.',
+    'about': 'Ищу работу html-верстальщиком, в этой профессии я нашла применение моим навыкам: усидчивость, внимательность к деталям. Помимо этого мне нравится верстать, я могу часами работать, забывая сделать перерыв на чай. Через несколько лет вижу себя front-end разработчиком. Для этого я повышаю уровень владения английским языком и изучаю JavaScript. Вот небольшая <a href="https://kseniyayaskevich.github.io/kseniyayaskevich-JSFEPRESCHOOL/memory-game/" target="_blank">ИГРА</a> для памяти. Я стремлюсь получить больше опыта в этой области. По этой причине я ищу компанию, готовую предложить мне место среди своих разработчиков. Взамен я бы предложила свою полную приверженность и была бы ответственным и дружелюбным дополнением к вашей команде.',
   }
 }
 
